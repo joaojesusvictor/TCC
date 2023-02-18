e844159c4a39186193b2bdc6d0bc652295f0bd03
João Victor <joaojesusvictor7@gmail.com>
2023-02-18 18:43:42 -0300

    Inicio Login
    - Inicio desenvolvimento login

---
+++ TechCompilerCo/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace TechCompilerCo.Models
+{
+    public class LoginViewModel
+    {
+        public LoginViewModel()
+        {
+            Usuario = string.Empty;
+            Senha = string.Empty;               //Construtor reclama se não tem prop
+            MsgErro = string.Empty;
+        }
+
+        public string Usuario { get; set; }
+        public string Senha { get; set; }
+        public string MsgErro { get; set; }
+    }
+}
---
+++ TechCompilerCo/Repositorys/LoginRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace TechCompilerCo.Repositorys
+{
+    public class LoginRepository : IRepository<Login>
+    {
+        public LoginRepository()
+        {
+
+        }
+
+        public void Add(Login obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Login obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Login obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Login
+    {
+
+    }
+}
